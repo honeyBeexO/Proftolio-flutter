e7d47dc0b4f5b59522d173e469f1e01b9b7d29e1
nouh ben kadi <benkadi.nouh@icloud.com>
2020-07-26 19:16:35 +0100

    🔥 add contact form

---
+++ lib/sections/contact/components/contact_box.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:portfolioweb/components/default_button.dart';
+import 'package:portfolioweb/constants.dart';
+import 'package:portfolioweb/sections/contact/components/social_card.dart';
+
+class ContactBox extends StatelessWidget {
+  const ContactBox({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(maxWidth: 1110.0),
+      padding: EdgeInsets.all(kDefaultPadding * 2.5),
+      margin: EdgeInsets.only(top: kDefaultPadding * 3),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30.0),
+          topRight: Radius.circular(30.0),
+        ),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SocialCard(
+                text: 'nouhben',
+                imageURL: 'images/skype.png',
+                color: Color(0xffD9FFFC),
+              ),
+              SocialCard(
+                text: 'nouhben',
+                imageURL: 'images/whatsapp.png',
+                color: Color(0xffE4FFC7),
+              ),
+              SocialCard(
+                text: 'nouhben',
+                imageURL: 'images/messanger.png',
+                color: Color(0xffE8F0F9),
+              ),
+            ],
+          ),
+          SizedBox(height: kDefaultPadding * 2),
+          ContactForm(),
+        ],
+      ),
+    );
+  }
+}
+
+class ContactForm extends StatelessWidget {
+  const ContactForm({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Wrap(
+        spacing: kDefaultPadding * 2.5,
+        runSpacing: kDefaultPadding * 1.5,
+        children: [
+          SizedBox(
+            width: 470.0,
+            child: TextFormField(
+              onChanged: (value) {
+                print(value);
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter your name',
+                labelText: 'Fullname',
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 470.0,
+            child: TextFormField(
+              onChanged: (value) {
+                print(value);
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter your email',
+                labelText: 'Email',
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 470.0,
+            child: TextFormField(
+              onChanged: (value) {
+                print(value);
+              },
+              decoration: InputDecoration(
+                hintText: 'Select project type',
+                labelText: 'Project Type',
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 470.0,
+            child: TextFormField(
+              onChanged: (value) {
+                print(value);
+              },
+              decoration: InputDecoration(
+                hintText: 'Select project budget',
+                labelText: 'Project Budget',
+              ),
+            ),
+          ),
+          SizedBox(
+            //height: 300.0,
+            child: TextFormField(
+              onChanged: (value) {
+                print(value);
+              },
+              decoration: InputDecoration(
+                hintText: 'Write a description ... ',
+                labelText: 'Description',
+              ),
+            ),
+          ),
+          //SizedBox(height: kDefaultPadding * 2),
+          Center(
+            child: FittedBox(
+              child: DefaultButton(
+                imageURL: 'images/contact_icon.png',
+                text: 'Contact me',
+                onTap: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/sections/contact/components/social_card.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:portfolioweb/constants.dart';
+
+class SocialCard extends StatefulWidget {
+  final String text;
+  final String imageURL;
+  final Color color;
+  final Function onPress;
+  const SocialCard({
+    this.onPress,
+    this.color,
+    this.imageURL,
+    this.text,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _SocialCardState createState() => _SocialCardState();
+}
+
+class _SocialCardState extends State<SocialCard> {
+  Duration _duration = Duration(microseconds: 200);
+  bool _isHovered = false;
+  //1- Animated container
+  //2- Inkwell
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      child: InkWell(
+        onTap: widget.onPress,
+        onHover: (value) {
+          setState(() {
+            _isHovered = value;
+          });
+        },
+        hoverColor: Colors.transparent,
+        child: AnimatedContainer(
+          duration: _duration,
+          padding: EdgeInsets.symmetric(
+            vertical: kDefaultPadding / 2,
+            horizontal: kDefaultPadding * 1.5,
+          ),
+          decoration: BoxDecoration(
+            color: this.widget.color, //Color(0xffD9FFFC),
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              if (_isHovered) kDefaultCardShadow,
+            ],
+          ),
+          child: Row(
+            children: [
+              Image.asset(
+                this.widget.imageURL ?? 'images/email.png',
+                width: 80.0,
+                height: 80.0,
+              ),
+              SizedBox(width: kDefaultPadding),
+              Text(this.widget.text ?? 'nouhben/')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
